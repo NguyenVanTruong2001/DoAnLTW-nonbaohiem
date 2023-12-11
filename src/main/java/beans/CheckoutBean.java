2ceaaae92973efac7c6aab8c46c468e8c1b0e86b
NguyenVanTruong2001 <19130256@st.hcmuaf.edu.vn>
2023-12-11 22:34:03 +0700

    add bean classes for checkout function

---
+++ src/main/java/beans/CheckoutBean.java
@@ -0,0 +1,90 @@
+package beans;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class CheckoutBean implements Serializable {
+    private int orderId;
+    private int userId;
+    private Date orderDate;
+    private String fullname;
+    private String telephone;
+    private String address;
+    private String paymentMethod;
+    private String orderState;
+
+    public CheckoutBean(int orderId, int userId, Date orderDate, String fullname, String telephone, String address, String paymentMethod, String orderState) {
+        this.orderId = orderId;
+        this.userId = userId;
+        this.orderDate = orderDate;
+        this.fullname = fullname;
+        this.telephone = telephone;
+        this.address = address;
+        this.paymentMethod = paymentMethod;
+        this.orderState = orderState;
+    }
+
+    public int getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(int orderId) {
+        this.orderId = orderId;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public Date getOrderDate() {
+        return orderDate;
+    }
+
+    public void setOrderDate(Date orderDate) {
+        this.orderDate = orderDate;
+    }
+
+    public String getFullname() {
+        return fullname;
+    }
+
+    public void setFullname(String fullname) {
+        this.fullname = fullname;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPaymentMethod() {
+        return paymentMethod;
+    }
+
+    public void setPaymentMethod(String paymentMethod) {
+        this.paymentMethod = paymentMethod;
+    }
+
+    public String getOrderState() {
+        return orderState;
+    }
+
+    public void setOrderState(String orderState) {
+        this.orderState = orderState;
+    }
+}
