912473ada8f80eb01278374c01a8db83b1e8d60d
NguyenVanTruong2001 <19130256@st.hcmuaf.edu.vn>
2023-12-14 00:01:32 +0700

    checkout success

---
+++ src/main/java/controllers/CheckoutSuccessController.java
@@ -0,0 +1,24 @@
+package controllers;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/checkout-success")
+public class CheckoutSuccessController extends HttpServlet {
+    public CheckoutSuccessController() {}
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getRequestDispatcher("checkoutSuccess.jsp").forward(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doGet(req, resp);
+    }
+}
