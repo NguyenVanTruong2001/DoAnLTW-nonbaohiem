309913d695a9e3acf478fccad55bb3a86a08f937
NguyenVanTruong2001 <19130256@st.hcmuaf.edu.vn>
2023-12-15 12:22:57 +0700

    more work to checkout history

--- src/main/java/dao/CheckoutDetailDao.java
+++ src/main/java/dao/CheckoutDetailDao.java
@@ -1,10 +1,12 @@
 package dao;
 
+import beans.CheckoutBean;
 import beans.CheckoutDetailBean;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 public class CheckoutDetailDao {
     public void addCheckoutDetail(CheckoutDetailBean checkoutDetail) throws SQLException {
@@ -18,5 +20,53 @@ public class CheckoutDetailDao {
         statement.setInt(3, checkoutDetail.getQuantity());
 
         statement.executeUpdate();
+    }
+
+    public List<CheckoutDetailBean> getAllCheckoutDetails() throws ClassNotFoundException, SQLException {
+        String sql = "SELECT * FROM OrderDetails";
+
+        Connection connection = new DBConnect().connect();
+
+        Statement statement = connection.createStatement();
+        ResultSet result = statement.executeQuery(sql);
+        List<CheckoutDetailBean> checkoutDetailList = new ArrayList<>();
+
+        while (result.next()) {
+            CheckoutDetailBean checkoutDetail = new CheckoutDetailBean(0, 0, 0);
+            checkoutDetail.setOrderId(result.getInt(1));
+            checkoutDetail.setProductId(result.getInt(2));
+            checkoutDetail.setQuantity(result.getInt(3));
+            checkoutDetailList.add(checkoutDetail);
+        }
+
+        connection.close();
+        return checkoutDetailList;
+    }
+
+    public List<CheckoutDetailBean> getCheckoutDetailByOrderId(int orderId) throws ClassNotFoundException, SQLException {
+        String sql = "SELECT * FROM OrderDetails WHERE `OrderID` = " + orderId;
+
+        Connection connection = new DBConnect().connect();
+
+        Statement statement = connection.createStatement();
+        ResultSet result = statement.executeQuery(sql);
+        List<CheckoutDetailBean> checkoutDetailList = new ArrayList<>();
+
+        while (result.next()) {
+            CheckoutDetailBean checkoutDetail = new CheckoutDetailBean(0, 0, 0);
+            checkoutDetail.setOrderId(result.getInt(1));
+            checkoutDetail.setProductId(result.getInt(2));
+            checkoutDetail.setQuantity(result.getInt(3));
+            checkoutDetailList.add(checkoutDetail);
+        }
+
+        connection.close();
+        return checkoutDetailList;
+    }
+
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+        for (CheckoutDetailBean bean: new CheckoutDetailDao().getCheckoutDetailByOrderId(1)) {
+            System.out.println(bean);
+        }
     }
 }
