ad7a3bf529291848c5a2e07084af4d37d80f46a9
NguyenVanTruong2001 <19130256@st.hcmuaf.edu.vn>
2023-12-17 11:19:32 +0700

    tweak DBConnect class

--- src/main/java/dao/DBConnect.java
+++ src/main/java/dao/DBConnect.java
@@ -8,21 +8,8 @@ public class DBConnect {
     protected static final String jdbcUrl = "jdbc:mysql://localhost:3306/HelmetManager";
     protected static final String dbUser = "root";
     protected static final String dbPass = "hello";
-    public Connection connect() {
-        Connection connection;
-
-        try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-        } catch (ClassNotFoundException e) {
-            throw new RuntimeException(e);
-        }
-
-        try {
-            connection = DriverManager.getConnection(jdbcUrl, dbUser, dbPass);
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-
-        return connection;
+    public Connection connect() throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        return DriverManager.getConnection(jdbcUrl, dbUser, dbPass);
     }
 }
