574f683e1ff3d5afa9a8f8eae42aae0855e2894b
NguyenVanTruong2001 <19130256@st.hcmuaf.edu.vn>
2024-01-17 00:54:24 +0700

    implement update user method

---
+++ src/main/java/controllers/UpdateUserController.java
@@ -0,0 +1,43 @@
+package controllers;
+
+import dao.CategoryDao;
+import dao.UserDao;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.SQLException;
+
+@WebServlet("/update-user")
+public class UpdateUserController extends HttpServlet {
+    public UpdateUserController() {}
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doPost(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.setCharacterEncoding("utf-8");
+        resp.setContentType("text/html;charset=utf-8");
+
+        int userId = Integer.parseInt(req.getParameter("userId"));
+        String username = req.getParameter("username");
+        String email = req.getParameter("email");
+        String password = req.getParameter("password");
+        String role = req.getParameter("role");
+        int i;
+
+        try {
+            i = new UserDao().updateUserById(userId, username, email, password, role);
+            if (i > 0) resp.sendRedirect("user-management");
+        } catch (ClassNotFoundException | SQLException e) {
+            req.setAttribute("message", "Thao tác không thành công. Hãy thử lại lần sau.");
+            req.getRequestDispatcher("form-user").forward(req, resp);
+        }
+    }
+}
