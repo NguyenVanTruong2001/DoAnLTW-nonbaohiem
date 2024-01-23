8b47ea8301158355605b02606bcbc4cadbbb0cdd
NguyenVanTruong2001 <19130256@st.hcmuaf.edu.vn>
2024-01-23 22:52:19 +0700

    implement get profit methods to controller

--- src/main/java/controllers/DashboardController.java
+++ src/main/java/controllers/DashboardController.java
@@ -9,6 +9,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.sql.SQLException;
+import java.util.Calendar;
 
 @WebServlet("/dashboard")
 public class DashboardController extends HttpServlet {
@@ -21,14 +22,20 @@ public class DashboardController extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        int totalSoldProduct;
+        int totalSoldProduct, totalProfitThisMonthOfYear, totalProfitThisYear;
+        int thisMonth = Calendar.getInstance().get(Calendar.MONTH) + 1;
+        int thisYear = Calendar.getInstance().get(Calendar.YEAR);
         try {
             totalSoldProduct = new CheckoutDetailDao().getTotalSoldProduct();
+            totalProfitThisMonthOfYear = new CheckoutDetailDao().getProfitThisMonthOfYear(thisMonth, thisYear);
+            totalProfitThisYear = new CheckoutDetailDao().getProfitThisYear(thisYear);
         } catch (ClassNotFoundException | SQLException e) {
             throw new RuntimeException(e);
         }
 
         req.setAttribute("totalSoldProduct", totalSoldProduct);
+        req.setAttribute("totalProfitThisMonthOfYear", totalProfitThisMonthOfYear);
+        req.setAttribute("totalProfitThisYear", totalProfitThisYear);
         req.getRequestDispatcher("dashboard.jsp").forward(req, resp);
     }
 }
